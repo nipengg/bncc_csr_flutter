009ad619bc7d1bd85284fd8ddfb48ebc9ed479ad
nipengg <nevillecorneliustj@gmail.com>
2022-09-18 00:27:05 +0700

    init

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar'),
+        ),
+        body: const Text('Hello'),
+      ),
+    );
+  }
+}
